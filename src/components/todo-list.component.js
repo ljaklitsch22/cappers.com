fc4b825956639884382a9502515226590ae62cb9
Lukas <lukasjaklitsch22@gmail.com>
2020-03-21 11:07:41 -0400

    First Commit

---
+++ src/components/todo-list.component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { render } from '@testing-library/react';
+
+export default class TodosList extends Component{
+    render(){
+        return(
+            <div>
+                <p>Welcome to TodosList</p>
+            </div>
+        )
+    }
+}
